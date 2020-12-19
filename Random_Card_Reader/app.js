64dada6234b022eb7073ab0d9ab44599f36c4fa0
Jose Hurtado <hello@josehurtado.net>
2020-12-19 01:43:23 +0000

    Css and JS updates querySelector

---
+++ Random_Card_Reader/app.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    console.log("Hello World")
+}
+
+document.querySelector(".top-left").innerHTML = "&#9824";
+
+document.querySelector(".center").innerHTML = "5";
+
+document.querySelector(".top-right").innerHTML = "&#9824";
