3024d32df517e8d572897e2478841c541ec369e4
Jose Hurtado <hello@josehurtado.net>
2021-01-15 19:55:20 +0000

    Reload Button and CSS colors

--- Random_Card_Reader/app.js
+++ Random_Card_Reader/app.js
@@ -1,22 +1,29 @@
-window.onload = function() {
-    document.querySelector(".top-suit").innerHTML = generateRandomSuit();
-    document.querySelector(".bottom-suit").innerHTML = generateRandomSuit();
-    document.querySelector(".numbers").innerHTML = generateRandomNumber();
+window.onload = () => {
+
+  function generateRandomSuit() {
+    let suits = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+    return suits[Math.floor(Math.random() * suits.length)];
+  }
+  
+  function generateRandomNumber() {
+      let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+    {
+      return numbers[Math.floor(Math.random() * numbers.length)];
+    }
+  }
+  var design = generateRandomSuit();
+
+  if (design == "&hearts;" || design == "&diams;") {
+    document.querySelector(".top-suit").style.color = "red";
+    document.querySelector(".bottom-suit").style.color = "red";
+    document.querySelector(".numbers").style.color = "red";
+  } else if (suits) {
+    document.querySelector(".top-suit").style.color = "black";
+    document.querySelector(".bottom-suit").style.color = "black";
+    document.querySelector(".numbers").style.color = "black";
+  }
+
+  document.querySelector(".top-suit").innerHTML = design;
+  document.querySelector(".bottom-suit").innerHTML = design;
+  document.querySelector(".numbers").innerHTML = generateRandomNumber();
 };
-
-function generateRandomNumber() {
-    let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-    let indexNumbers = Math.floor(Math.random() * numbers.length);
-    console.log(numbers[indexNumbers]);
-    return numbers[indexNumbers];
-
-};
-
-function generateRandomSuit() {
-    let suits = ["&diams;", "&spades;", "&hearts;", "&clubs;"];
-    let indexSuit = Math.floor(Math.random() * suits.length);
-    console.log(suits[indexSuit]);
-    return suits[indexSuit];
-
-};
-
